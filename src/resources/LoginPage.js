bf437c8d94a101120d1bc6c2d5e581debf982e5c
JustMonk <night.fire@mail.ru>
2021-02-14 14:17:43 +0500

    add login page

---
+++ src/resources/LoginPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export function LoginPage(props) {
+   return <div>
+      <h2>Login page</h2>
+      <input placeholder="login" />
+      <input placeholder="password" />
+   </div>
+}
+
+export default LoginPage;
