4c4925411229d827dca36ddda8ee1ef2023f907d
JustMonk <night.fire@mail.ru>
2021-02-16 22:46:21 +0500

    add LoginPage styles

--- src/resources/LoginPage.js
+++ src/resources/LoginPage.js
@@ -1,10 +1,39 @@
 import React, { useState, useEffect } from "react";
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles(theme => ({
+   wrapper: {
+      background: '#e5e7eb',
+      maxWidth: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   loginCard: {
+      boxShadow: '0 0 #0000,0 0 #0000,0 0 #0000,0 0 #0000,0 4px 6px -1px rgba(0, 0, 0, 0.1),0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      background: '#fff',
+      width: '33%',
+      padding: '2rem',
+   }
+}));
 
 export function LoginPage(props) {
-   return <div>
-      <h2>Login page</h2>
-      <input placeholder="login" />
-      <input placeholder="password" />
+   const classes = useStyles();
+
+   return <div className={classes.wrapper}>
+      <div className={classes.loginCard}>
+         <h1>Login</h1>
+         <input placeholder="login" />
+         <input placeholder="password" />
+
+         <div>
+            <button>Login</button>
+            <div>OR</div>
+            <button>Register</button>
+         </div>
+
+      </div>
    </div>
 }
 
