42b61400c3587364c9bad935ae9084c339dd67cb
JustMonk <night.fire@mail.ru>
2021-02-20 23:54:25 +0500

    fix font

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -21,6 +21,7 @@ const useStyles = createUseStyles(theme => ({
       backgroundColor: '#fff',
       border: '0 solid #e2e8f0',
       width: '100%',
+      fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
       marginTop: '.5rem',
       '&:focus': {
          boxShadow: `0 0 0 3px ${theme.colorPrimary + '42'}`,
